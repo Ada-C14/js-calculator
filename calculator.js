007899885923fb64972a661fa142fa61bd03e81c
Kareha Agesa <kmaagesa@gmail.com>
2020-12-07 19:53:33 -0800

    npm tests passing

--- calculator.js
+++ calculator.js
@@ -1,17 +1,3 @@
-
-// const exampleAdditionInput = {
-//   num1: 3,
-//   num2: 5,
-//   operation: 'add',
-// }
-let input = {
-  num1: 3,
-  num2: 5,
-  operation: 'add',
-}
-
-calculateUserInput({}, input)
-
 function validNumber(number) {
   if (isNaN(parseInt(number))) {
     throw `${number} is not a number`;
@@ -20,23 +6,23 @@ function validNumber(number) {
 } 
 
 const calculateUserInput = function (error, promptInput) {
-
-
   const validOperations = ['add', 'subtract', 'multiply', 'divide', 'modulus', 'exponent', '+', '-', '*', '/', '%', '**'];
   const invalidZeroOperations = ["divide", "/", "modulus", "%"];
 
   // check that operator is valid
   if (!validOperations.includes(promptInput.operation)) {
-    throw 'Unsupported Operator'
+    // throw 'Unsupported Operator'
+    console.log('Unsupported Operator')
   }
 
   // check that numbers are valid
-  const num1 = validNumber(promptInput.num1)
-  const num2 = validNumber(promptInput.num2)
+  const num1 = parseInt(validNumber(promptInput.num1))
+  const num2 = parseInt(validNumber(promptInput.num2))
 
   // make sure not trying to divide/mod by 0
   if (invalidZeroOperations.includes(promptInput.operation) && num2 == 0) {
-    throw 'You cannot divide or take the modulus of a number by zero'
+    // throw 'You cannot divide or take the modulus of a number by zero'
+    console.log('You cannot divide or take the modulus of a number by zero')
   }
 
   // do the math
@@ -55,14 +41,6 @@ const calculateUserInput = function (error, promptInput) {
   }
 
 }
-
-// Example manual testing of calculator.  
-// calculateUserInput({}, {
-//   num1: 3,
-//   num2: 4,
-//   operation: 'add',
-// });
-
 
 // This exports the function so index.js can import it.
 exports.calculateUserInput = calculateUserInput;
