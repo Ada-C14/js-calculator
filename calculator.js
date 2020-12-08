ae08d3377333f0c9588bec64d63885e6c24c9094
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-12-07 23:42:27 -0800

    add addition functionality

--- calculator.js
+++ calculator.js
@@ -4,32 +4,35 @@ const exampleAdditionInput = {
   operation: 'add',
 }
 
-function getValidInput(operator) {
+function getValidOperator(operator) {
   const operators = ['add', '+', 'subtract', '-', 'multiply', '*', 'divide', '/'];
 
   if (operators.includes(operator)) {
+    return operator;
+  } else {
+    console.log(`${operator} is not a valid operator.`);
+  }
+}
 
+function getValidNumber(number) {
+  if (!isNaN(number)) {
+    return parseInt(number);
+  } else {
+    console.log(`${number} is not a valid number.`);
   }
 }
 
 const calculateUserInput = function (error, promptInput) {
   console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
 
-  // Questions to ask and answer:
-  // What is promptInput?
-  // What data type? What does it hold? What does it represent?
-  // How do we read values from it? What syntax?
-  // How can we use it?
-  // Can we call our existing functions now, inside of this function?
+  let num1 = getValidNumber(promptInput.num1);
+  let num2 = getValidNumber(promptInput.num2);
+  let operator = getValidOperator(promptInput.operation)
+
+  if (operator === 'add' || operator === '+') {
+    console.log(`${num1} + ${num2} = ${num1 + num2}`)
+  }
 }
-
-// Example manual testing of calculator.
-// calculateUserInput({}, {
-//   num1: 3,
-//   num2: 4,
-//   operation: 'add',
-// });
-
 
 // This exports the function so index.js can import it.
 exports.calculateUserInput = calculateUserInput;
