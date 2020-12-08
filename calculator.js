68ad8109918d2fa3f7bfb17218781c29520471d9
Anna Kim <anna.kim93@gmail.com>
2020-12-07 22:10:23 -0500

    manually testing calculator

--- calculator.js
+++ calculator.js
@@ -55,11 +55,11 @@ const calculateUserInput = function (error, promptInput) {
 }
 
 // Example manual testing of calculator.  
-// calculateUserInput({}, {
-//   num1: 3,
-//   num2: 4,
-//   operation: 'add',
-// });
+calculateUserInput({}, {
+  num1: 3,
+  num2: 4,
+  operation: 'add',
+});
 
 
 // This exports the function so index.js can import it.
