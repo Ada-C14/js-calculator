a37fa95a2a0f7c984f33fc318dbecb17196b1de2
Yingjing Lin <yjlin789@gmail.com>
2020-12-07 23:48:39 -0800

    pass all the tests

--- calculator.js
+++ calculator.js
@@ -1,14 +1,24 @@
 
 const inputSample = {
   num1: 3,
-  num2: 5,
-  operation: 'add',
+  num2: 6,
+  operation: '+',
 }
 
 const calculateUserInput = function (error, input) {
   const operation = input.operation;
-  const a = input.num1
-  const b = input.num2
+
+  if (isNaN(input.num1)) {
+    console.log(`${input.num1} is not a number`)
+    return
+  }
+  const a = Number(input.num1)
+
+  if (isNaN(input.num2)) {
+    console.log(`${input.num2} is not a number`)
+    return
+  }
+  const b = Number(input.num2)
 
   switch (operation) {
     case '+':
@@ -21,12 +31,18 @@ const calculateUserInput = function (error, input) {
       break;
     case '*':
     case 'multiply':
-      console.log(`${a} + ${b} = ${a + b}`);
+      console.log(`${a} * ${b} = ${a * b}`);
       break;
     case '/':
     case 'divide': 
+      if (b === 0) {
+        console.log('You cannot divide by zero');
+        break;
+      }
       console.log(`${a} / ${b} = ${a / b}`);
       break;
+    default:
+      console.log('unsupported operator');
   }
 }
 
