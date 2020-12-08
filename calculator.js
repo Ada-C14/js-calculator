3c279ff9e2ca7030ffcb67afbf8bd02b82eb15e0
MonaRahmani <monarahmanimehr@gmail.com>
2020-12-07 19:44:00 -0800

    edited

--- calculator.js
+++ calculator.js
@@ -12,18 +12,18 @@ const add = function(num1, num2) {
 }
 
 const subtract = function(num1, num2) {
-  console.log(`${num1} - ${num2} = ${num1 - num2}`);
+  console.log(`${num1} - ${num2} = ${Number(num1) - Number(num2)}`);
 }
 
 const multiply = function(num1, num2) {
-  console.log(`${num1} * ${num2} = ${num1 * num2}`);
+  console.log(`${num1} * ${num2} = ${Number(num1) * Number(num2)}`);
 }
 
 const divide = function(num1, num2) {
   if (num2 == 0) {
     console.log("you can not divide by zero!")
   } else {
-    console.log(`${num1} / ${num2} = ${num1 / num2}`) 
+    console.log(`${num1} / ${num2} = ${Number(num1) / Number(num2)}`) 
   }
   
 }
