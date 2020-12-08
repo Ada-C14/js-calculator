bd3017fbaf70563a3538db768c1110ca68ad75a8
Yingjing Lin <yjlin789@gmail.com>
2020-12-07 22:58:08 -0800

    add basic operation

--- calculator.js
+++ calculator.js
@@ -1,20 +1,35 @@
 
-const exampleAdditionInput = {
+const inputSample = {
   num1: 3,
   num2: 5,
   operation: 'add',
 }
 
-const calculateUserInput = function (error, promptInput) {
-  console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
+const calculateUserInput = function (error, input) {
+  const operation = input.operation;
+  const a = input.num1
+  const b = input.num2
 
-  // Questions to ask and answer:
-  // What is promptInput?
-  // What data type? What does it hold? What does it represent?
-  // How do we read values from it? What syntax?
-  // How can we use it?
-  // Can we call our existing functions now, inside of this function?
+  switch (operation) {
+    case '+':
+    case 'add': 
+      console.log(`${a} + ${b} = ${a + b}`);
+      break;
+    case '-':
+    case 'subtract':
+      console.log(`${a} - ${b} = ${a - b}`);
+      break;
+    case '*':
+    case 'multiply':
+      console.log(`${a} + ${b} = ${a + b}`);
+      break;
+    case '/':
+    case 'divide': 
+      console.log(`${a} / ${b} = ${a / b}`);
+      break;
+  }
 }
+
 
 // Example manual testing of calculator.  
 // calculateUserInput({}, {
@@ -22,7 +37,7 @@ const calculateUserInput = function (error, promptInput) {
 //   num2: 4,
 //   operation: 'add',
 // });
-
+calculateUserInput(null, inputSample)
 
 // This exports the function so index.js can import it.
 exports.calculateUserInput = calculateUserInput;
