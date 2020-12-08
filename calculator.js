31b469f3082d5014c7b9accb91b90dd5139708ac
Alice D <alicedilanchian@gmail.com>
2020-12-07 21:35:02 -0500

    program bugs fixed; works correctly

--- calculator.js
+++ calculator.js
@@ -3,55 +3,27 @@ const calculateUserInput = function (error, promptInput) {
 
   // Questions to ask and answer:
   // What is promptInput?
+  // --> It is the equivalent of gets.chomp in ruby
   // What data type? What does it hold? What does it represent?
+  // -> Data type is the equivalent of class in ruby. It holds a value such as 'string', 'number'. It reprensents the data type of a object.
   // How do we read values from it? What syntax?
+  // -> typeof variableName
   // How can we use it?
+  // -> I used it to check if the num1 and num2 were numbers, i.e.
+  //if ('number' !== typeof input['num1']){
+  //  console.log(`Error: "${input['num1']}" is not a number. Please enter a number and run the program again.`);
+  //}
   // Can we call our existing functions now, inside of this function?
+  // -> Yes
+  // const answer = calculateUserInput(input);
+  // console.log(answer);
 }
 
 
 // This exports the function so index.js can import it.
 exports.calculateUserInput = calculateUserInput;
 
-const input = {
-  num1: 3,
-  num2: 5,
-  operation: 'add',
-}
-
-// const addNums = function(a,b){
-//   const result = a + b;
-//   return result;
-// }
-
-// const subtractNums = function(a,b){
-//   const result = a - b;
-//   return result;
-// }
-
-// const multiplyNums = function(a,b){
-//   const result = a * b;
-//   return result;
-// }
-
-// const divideNums = function(a,b){
-//   const result = a / b;
-//   return result;
-// }
-
-// const modulusNums = function(a,b){
-//   const result = a % b;
-//   return result;
-// }
-
-
-const calculator = function(input){
-
-  // const validOperations = {'+': '+', 'add': '+', '-': '-', 'subtract': '-', '*': '*', '*': 'multiply', '/': 'divide', '/': '/'};
-
-  // if ((input[operation] in validOperations) === false){
-  //   console.log(`Error: "${input[operation]}" is not a valid operator. Please enter a valid operator and run the program again.`);
-  // }
+const calculateUserInput = function(input){
 
   if ('number' !== typeof input['num1']){
     console.log(`Error: "${input['num1']}" is not a number. Please enter a number and run the program again.`);
@@ -60,41 +32,60 @@ const calculator = function(input){
   if ('number' !== typeof input['num2']){
     console.log(`Error: "${input['num2']}" is not a number. Please enter a number and run the program again.`);
   }
-let result;
 
-switch (input['operation']){
-  case 'add' || '+':
-    result = input['num1'] + input['num2'];
-    break;
-  case 'subtract' || '-':
-    result = input['num1'] - input['num2'];
-    break;
-  case 'multiply' || '*':
-    result = input['num1'] * input['num2'];
-    break;
-  case 'divide' || '/':
-    if (input['num2'] === 0){
-      result = 'cannot be calculated because we cannot divide by zero';
-    }else{
-      result = input['num1'] / input['num2'];
-    }
-    break;
-  case'modulus' || '%':
-    if (input['num2'] === 0){
-      result = 'cannot be calculated because we cannot perform modulus zero';
-    }else{
-      result = input['num1'] % input['num2'];
-    }
-    break;
-  case 'raised-to' || '^':
-    result = input['num1'] ** input['num2'];
-    break;
-  default:
-    result = `The word or symbol "$input[operator]}" is not a valid operator.  Please enter a valid operator, and run the program again.`;
+  let result;
+
+  switch (input['operation']){
+    case 'add':
+    case '+':
+      result = (input['num1'] + input['num2']);
+      console.log(`${input['num1']} + ${input['num2']} = ${result}`);
+      break;
+    case 'subtract':
+    case '-':
+      result = (input['num1'] - input['num2']);
+      console.log(`${input['num1']} - ${input['num2']} = ${result}`);
+      break;
+    case 'multiply':
+    case '*':
+      result = (input['num1'] * input['num2']);
+      console.log(`${input['num1']} * ${input['num2']} = ${result}`);
+      break;
+    case 'divide':
+    case '/':
+      if (input['num2'] === 0){
+        result = 'cannot be calculated because we cannot divide by zero';
+      }else{
+        result = (input['num1'] / input['num2']);
+        console.log(`${input['num1']} / ${input['num2']} = ${result}`);
+      }
+      break;
+    case'modulus':
+    case '%':
+      if (input['num2'] === 0){
+        result = 'cannot be calculated because we cannot perform modulus zero';
+      }else{
+        result = (input['num1'] % input['num2']);
+        console.log(`${input['num1']} % ${input['num2']} = ${result}`);
+      }
+      break;
+    case 'raised-to':
+    case '^':
+      result = (input['num1'] ** input['num2']);
+      console.log(`${input['num1']} ^ ${input['num2']} = ${result}`);
+      break;
+    default:
+      result = `The word or symbol "${input['operation']}" is not a valid operator.  Please enter a valid operator, and run the program again.`;
+  }
+
+  return result;
 }
 
-return result;
-}
+// const input = {
+//   num1: 3,
+//   num2: -5,
+//   operation: '/',
+// }
 
-const answer = calculator(input);
-console.log(answer);
+// const answer = calculator(input);
+// console.log(answer);
