711ad5c4470e08dd3a21e8dc28e81207e849a7c3
Jasmine <jasylop@gmail.com>
2020-12-08 13:23:33 -0800

    answered questions

--- calculator.js
+++ calculator.js
@@ -9,11 +9,12 @@ const calculateUserInput = function (error, promptInput) {
   // console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
 
   // Questions to ask and answer:
-  // What is promptInput? => an object (hash)
-  // What data type? What does it hold? What does it represent?
-  // How do we read values from it? What syntax?
-  // How can we use it?
-  // Can we call our existing functions now, inside of this function?
+  // What is promptInput? => an argument
+  // What data type? What does it hold? What does it represent? => it's an object like a hash, it holds the data we want, num1, num2, and operator. It represents user input.
+  // How do we read values from it? What syntax? => we can use dot notation or bracket notation
+  // How can we use it? => We need to grab use it by grabbing num1, num2, and operator and creating the necessary calculations.
+  // Can we call our existing functions now, inside of this function? => not sure what this is asking
+
   const num1 = Number(promptInput.num1);
   const num2 = Number(promptInput.num2);
   const operator = promptInput.operation;
@@ -45,16 +46,12 @@ const calculateUserInput = function (error, promptInput) {
   }
 };
 
-
-
-
 // Example manual testing of calculator.  
 // calculateUserInput({}, {
 //   num1: "5",
 //   num2: 0,
 //   operation: '/hello',
 // });
-
 
 // This exports the function so index.js can import it.
 exports.calculateUserInput = calculateUserInput;
