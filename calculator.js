25b6f48760e60d228209f652b9abb8542efe1114
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-12-08 00:28:47 -0800

    refactor from if-else to switch in calculateUserInput method

--- calculator.js
+++ calculator.js
@@ -1,46 +1,60 @@
-const exampleAdditionInput = {
-  num1: 3,
-  num2: 5,
-  operation: 'add',
-}
-
 function getValidOperator(operator) {
   const operators = ['add', '+', 'subtract', '-', 'multiply', '*', 'divide', '/'];
 
   if (operators.includes(operator)) {
     return operator;
-  } else {
-    console.log(`${operator} is not a valid operator.`);
   }
+
+  return undefined;
 }
 
 function getValidNumber(number) {
   if (!isNaN(number)) {
     return parseInt(number);
-  } else {
-    console.log(`${number} is not a valid number.`);
   }
+
+  return undefined;
 }
 
 const calculateUserInput = function (error, promptInput) {
   console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
 
   let num1 = getValidNumber(promptInput.num1);
-  let num2 = getValidNumber(promptInput.num2);
-  let operator = getValidOperator(promptInput.operation);
+  if (num1 == null) {
+    console.log('invalid num1: ' + promptInput.num1);
+  }
 
-  if (operator === 'add' || operator === '+') {
-    console.log(`${num1} + ${num2} = ${num1 + num2}`);
-  } else if (operator === 'subtract' || operator === '-') {
-    console.log(`${num1} - ${num2} = ${num1 - num2}`);
-  } else if (operator === 'multiply' || operator === '*') {
-    console.log(`${num1} * ${num2} = ${num1 * num2}`);
-  } else if (operator === 'divide' || operator === '/') {
-    if (num2 == 0) {
-      console.log('You cannot divide by zero');
-    } else {
+  let num2 = getValidNumber(promptInput.num2);
+  if (num2 == null) {
+    console.log('invalid num2: ' + promptInput.num2);
+  }
+
+  let operator = getValidOperator(promptInput.operation);
+  if (operator == null) {
+    console.log('invalid operator: ' + promptInput.operation);
+  }
+
+  switch (operator) {
+    case 'add':
+    case '+':
+      console.log(`${num1} + ${num2} = ${num1 + num2}`);
+      break;
+    case 'subtract':
+    case '-':
+      console.log(`${num1} - ${num2} = ${num1 - num2}`);
+      break;
+    case 'multiply':
+    case '*':
+      console.log(`${num1} * ${num2} = ${num1 * num2}`);
+      break;
+    case 'divide':
+    case '/':
+      if (num2 == 0) {
+        console.log('You cannot divide by zero');
+      }
+
       console.log(`${num1} / ${num2} = ${num1 / num2}`);
-    }
+      break;
   }
 }
 
