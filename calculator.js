7f637554b6d5103626ce86c3f5ee43786d0c1b9c
Kareha Agesa <kmaagesa@gmail.com>
2020-12-08 09:41:21 -0800

    i forgot so many semi colons

--- calculator.js
+++ calculator.js
@@ -1,7 +1,6 @@
 function validNumber(number) {
   if (isNaN(parseInt(number))) {
-    // throw `${number} is not a number`;
-    console.log(`${number} is not a number`)
+    console.log(`${number} is not a number`);
     }
   return number;
 } 
@@ -12,33 +11,31 @@ const calculateUserInput = function (error, promptInput) {
 
   // check that operator is valid
   if (!validOperations.includes(promptInput.operation)) {
-    // throw 'Unsupported Operator'
-    console.log('Unsupported Operator')
+    console.log('Unsupported Operator');
   }
 
   // check that numbers are valid
-  const num1 = parseInt(validNumber(promptInput.num1))
-  const num2 = parseInt(validNumber(promptInput.num2))
+  const num1 = parseInt(validNumber(promptInput.num1));
+  const num2 = parseInt(validNumber(promptInput.num2));
 
   // make sure not trying to divide/mod by 0
   if (invalidZeroOperations.includes(promptInput.operation) && num2 == 0) {
-    // throw 'You cannot divide or take the modulus of a number by zero'
-    console.log('You cannot divide or take the modulus of a number by zero')
+    console.log('You cannot divide or take the modulus of a number by zero');
   }
 
   // do the math
   if (promptInput.operation == 'add' || promptInput.operation == '+') {
-    console.log(`${num1} + ${num2} = ${num1 + num2}`)
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
   } else if (promptInput.operation == 'subtract' || promptInput.operation == '-') {
-    console.log(`${num1} - ${num2} = ${num1 - num2}`)
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);
   } else if (promptInput.operation == 'multiply' || promptInput.operation == '*') {
-    console.log(`${num1} * ${num2} = ${num1 * num2}`)
+    console.log(`${num1} * ${num2} = ${num1 * num2}`);
   } else if (promptInput.operation == 'divide' || promptInput.operation == '/') {
-    console.log(`${num1} / ${num2} = ${num1 / num2}`)
+    console.log(`${num1} / ${num2} = ${num1 / num2}`);
   } else if (promptInput.operation == 'modulus' || promptInput.operation == '%') {
-    console.log(`${num1} % ${num2} = ${num1 % num2}`)
+    console.log(`${num1} % ${num2} = ${num1 % num2}`);
   } else {
-    console.log(`${num1} ^ ${num2} = ${num1 ** num2}`)
+    console.log(`${num1} ^ ${num2} = ${num1 ** num2}`);
   }
 
 }
