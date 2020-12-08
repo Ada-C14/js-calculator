a23ad2ee432711285dca99939d0296de9a316773
christinaminh <christina.minh@gmail.com>
2020-12-08 15:30:41 -0800

    Passed tests. Fixed conditionals where 0 === false.

--- calculator.js
+++ calculator.js
@@ -1,21 +1,40 @@
 // Helper methods
-const add = function(num1, num2) {
-  console.log(num1 + num2);
+const operatorSign = function(operation) {
+  switch (operation) {
+    case 'add':
+    case '+':
+      return '+';
+    case 'subtract':
+    case '-':
+      return '-';
+    case 'multiply':
+    case '*':
+      return '*';
+    case 'divide':
+    case '/':  
+      return '/';
+    default: 
+      return null;
+  }
 }
 
-const substract = function(num1, num2) {
-  console.log(num1 - num2);
+const addNums = function(num1, num2) {
+  return num1 + num2;
 }
 
-const multiply = function(num1, num2) {
-  console.log(num1 * num2);
+const substractNums = function(num1, num2) {
+  return num1 - num2;
 }
 
-const divide = function(num1, num2) {
+const multiplyNums = function(num1, num2) {
+  return num1 * num2;
+}
+
+const divideNums = function(num1, num2) {
   if (num2 == 0) {
-    console.log('You cannot divide by 0');
+    console.log('You cannot divide by zero');
   } else {
-    console.log(num1 / num2);
+    return num1 / num2;
   }
 }
 
@@ -23,12 +42,13 @@ const isNumber = function(num) {
   if (isNaN(num)) {
     console.log(`${num} is not a number`);
   } else if (parseFloat(num)) {
+    return true; 
+  } else if (num === 0 || num === '0') {
     return true
   } else {
-    return console.log('Empty string cannot be operated on. Number must be entered')
+    console.log('Empty string cannot be operated on. Number must be entered');
   }
 }
-
 
 // Example userInput
 const userInput = {
@@ -50,37 +70,36 @@ const calculateUserInput = function (error, promptInput) {
   if (isNumber(promptInput.num1) && isNumber(promptInput.num2)){
     const num1 = Number(promptInput.num1);
     const num2 = Number(promptInput.num2);
-    const operation = promptInput.operation;
+    const operator = operatorSign(promptInput.operation);
+    
+    const result = (function (operator){
+      switch (operator) {
+        case '+':
+          return addNums(num1, num2);
+        case '-':
+          return substractNums(num1, num2);
+        case '*':
+          return multiplyNums(num1, num2);
+        case '/':
+          return divideNums(num1, num2);
+        default:
+          console.log(`unsupported operator`);   
+      }
+    })(operator);
 
-    switch (operation) {
-      case 'add':
-      case '+':
-        add(num1, num2);
-        break;
-      case 'subtract':
-      case '-':
-        substract(num1, num2);
-        break;
-      case 'multiply':
-      case '*':
-        multiply(num1, num2);
-        break;
-      case 'divide':
-      case '/':
-        divide(num1, num2);
-        break;
-      default:
-        console.log(`unsupported operator`);
+
+    if (typeof result === 'number') {
+      console.log(`${num1} ${operator} ${num2} = ${result}`);
     }
   }
 }
 
-// Example manual testing of calculator.  
-// calculateUserInput({}, {
-//   num1: 'hotdog',
-//   num2: '',
-//   operation: '+',
-// });
+// // Example manual testing of calculator.  
+calculateUserInput({}, {
+  num1: 3,
+  num2: 0,
+  operation: '*',
+});
 
 
 // This exports the function so index.js can import it.
