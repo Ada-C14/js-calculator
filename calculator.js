ae96c6e9f8a5fd2b8ad04b8259afe718d521fb4f
Pauline Chane <pauline.chane@gmail.com>
2020-12-07 23:00:31 -0800

    added num1/2 error input validations, working on switch case for operation

--- calculator.js
+++ calculator.js
@@ -1,20 +1,39 @@
 
 const exampleAdditionInput = {
-  num1: 3,
+  num1: 'fake',
   num2: 5,
   operation: 'add',
 }
 
 const calculateUserInput = function (error, promptInput) {
   console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
+  
+  // constant holds key/value pairs to all valid operations
+  const operationObject = { add: ['+', 'add', 'addition', 'plus'], 
+                            subtract: ['-', 'subtract', 'subtraction', 'minus'],
+                            multiply: ['*', 'x', 'times', 'multiply', 'multiplication'],
+                            divide: ['/', 'divide', 'division'],
+                            mod: ['modulus', 'modulo', 'remainder', '%', 'mod'],
+                            exponent: ['power', '**', '^', 'exponent']};
 
-  // Questions to ask and answer:
-  // What is promptInput?
-  // What data type? What does it hold? What does it represent?
-  // How do we read values from it? What syntax?
-  // How can we use it?
-  // Can we call our existing functions now, inside of this function?
+  // check that num1 and num2 are valid numbers 
+  // note that we don't have parenthetical support yet
+  if(isNaN(promptInput.num1))
+  {
+    error.num1 = 'num1 must be valid number. no parenthetical support.' ;
+  }
+
+  if(isNaN(promptInput.num2))
+  {
+    error.num2 = 'num2 must be valid number. no parenthetical support.' ;
+  }
+
+  switch(promptInput.operation) {
+    
+  }
 }
+
+
 
 // Example manual testing of calculator.  
 // calculateUserInput({}, {
