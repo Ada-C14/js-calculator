315162e7f596d2bf0c996073359e39a9660e0b36
Jasmine <jasylop@gmail.com>
2020-12-08 13:20:28 -0800

    added spacing for readability

--- calculator.js
+++ calculator.js
@@ -16,27 +16,34 @@ const calculateUserInput = function (error, promptInput) {
   // Can we call our existing functions now, inside of this function?
   const num1 = Number(promptInput.num1);
   const num2 = Number(promptInput.num2);
-  const operator = promptInput.operation
+  const operator = promptInput.operation;
 
   if (isNaN(num1)) {
     console.log(`${promptInput.num1} is not a valid number`);
+
   } else if (isNaN(num2)) {
-    console.log(`${promptInput.num1} is not a valid number`)
+    console.log(`${promptInput.num1} is not a valid number`);
+
   } else {
+    
     if (operator === '+' || operator === 'add') {
       console.log(`${num1} + ${num2} = ${num1 + num2}`);
+
     } else if (operator === '-' || operator === 'subtract') {
       console.log(`${num1} - ${num2} = ${num1 - num2}`);
+
     } else if (operator === '*' || operator === 'multiply') {
       console.log(`${num1} * ${num2} = ${num1 * num2}`);
+
     } else if (operator === '/' || operator === 'divide') {
       if (num2 === 0) console.log('You cannot divide by zero');
       else console.log(`${num1} / ${num2} = ${num1 / num2}`);
+
     } else {
       console.log(`${operator} is not a valid operator`);
     }
   }
-}
+};
 
 
 
