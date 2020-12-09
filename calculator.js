dddfcc7211c4d2f72a0d07f1417a4b64a6fe4593
Iris Lux <iris.lux0@gmail.com>
2020-12-08 22:37:45 -0800

    basic calculator functionalit acheived" "/

--- calculator.js
+++ calculator.js
@@ -1,12 +1,14 @@
 
+var this_is_var = true;
+
 const exampleAdditionInput = {
   num1: 3,
-  num2: 5,
-  operation: 'add',
+  num2: 0,
+  operation: 'divide',
 }
 
 const calculateUserInput = function (error, promptInput) {
-  console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
+  //console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
 
   // Questions to ask and answer:
   // What is promptInput?
@@ -14,7 +16,61 @@ const calculateUserInput = function (error, promptInput) {
   // How do we read values from it? What syntax?
   // How can we use it?
   // Can we call our existing functions now, inside of this function?
+
+  const operations = {
+    '+': function (num1, num2){ return num1 + num2},
+    '-': function (num1, num2){ return num1 - num2},
+    '*': function (num1, num2){ return num1 * num2},
+    '/': function (num1, num2){ 
+      if(num2 === 0){
+        return 'zerodiv';
+      }
+      else{
+        return num1 / num2;
+      }
+    }
+  };
+
+  const operators = {
+    add: '+',
+    '+' : '+',
+    subtract: '-',
+    '-' : '-',
+    multiply: '*',
+    '*' : '*',
+    'x' : '*',
+    divide: '/',
+    '/': '/', 
+  }
+
+  const operator = operators[promptInput.operation.toLowerCase()]
+  const operation = operations[operator]
+  //const solution = operationsWithWords[promptInput.operation.toLowerCase()](promptInput.num1, promptInput.num2)
+
+  if(operation === undefined){
+    console.log('Operation is invalid.');
+  }
+  else{
+    const solution = operation(parseInt(promptInput.num1), parseInt(promptInput.num2));
+
+    if(solution === 'zerodiv'){
+      console.log('You cannot divide by zero');
+    }
+    else{
+      console.log(`${promptInput.num1} ${operator} ${promptInput.num2} = ${solution}`);
+    }
+
+  } 
+  
+
+
+  
 }
+
+
+calculateUserInput(1, exampleAdditionInput);
+
+
 
 // Example manual testing of calculator.  
 // calculateUserInput({}, {
