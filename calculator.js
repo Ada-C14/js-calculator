3a19b23003f6d1a06c6c72b692eb03ca8dd84a48
Rachael Gomez <gomezrc1220@gmail.com>
2020-12-09 12:02:03 -0800

    Finally working and passing all tests

--- calculator.js
+++ calculator.js
@@ -1,64 +1,61 @@
 
-// const exampleAdditionInput = {
-//   num1: 3,
-//   num2: 5,
-//   operation: 'add',
-// }
-// '3 + 5 = 8'
-
-// const calculateUserInput = function (error, promptInput) {
-//   console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
-
-  // Questions to ask and answer:
-  // What is promptInput?
-  // What data type? What does it hold? What does it represent?
-  // How do we read values from it? What syntax?
-  // How can we use it?
-  // Can we call our existing functions now, inside of this function?
-
-
-// Example manual testing of calculator.  
-// calculateUserInput({}, {
-//   num1: 3,
-//   num2: 4,
-//   operation: 'add',
-// });
-const calculateUserInput = function(userInput){
-	let num1 = userInput.num1
-	let num2 = userInput.num2
-  if (userInput.num1 === NaN || userInput.num2 === NaN);
-  {
-    console.log('Uh oh! Looks like you inputted something other than a number');
+const calculateAdd = function(input) {
+  let result = input.num1 + input.num2
+  console.log(`${input.num1} + ${input.num2} = ${result}`);
+   return;
+};
+const calculateSub = function(input ){
+  let result = input.num1 - input.num2
+  return console.log(`${input.num1} - ${input.num2} = ${result}`);
+  return;
+};
+const calculateDiv = function(input) {
+  if (input.num1 === 0 || input.num2 === 0){
+  console.log('You cannot divide by zero');
+   return;
+  }
+  let result = input.num1 / input.num2
+  console.log(`${input.num1} / ${input.num2} = ${result}`);
+   return;
+};
+const calculateMult = function(input) {
+  let result = input.num1 * input.num2
+  console.log(`${input.num1} * ${input.num2} = ${result}`);
+  return;
   };
-    
-  if (userInput.operation === 'add' || userInput.operation === '+');
- {
-  let result = num1 + num2
-  let operator = '+'
-		} else if (userInput.operation == 'subtract' || userInput.operation === '-');
-{
-  let result = num1 - num2
-  let operator = '-'
-		} else if (userInput.operation == 'multiply' || userInput.operation === '*');
-{
-  let result = num1 * num2
-  let operator = '*' 
-		} else if (userInput.operation == 'divide' || userInput.operation === '/');
-{
-  			if (num1 === 0 || num2 === 0);
-  				{
-    				console.log('Ummmm... you cannot divide by zero!');
-  				};
 
-				let result = num1 / num2
-				let operator = '/'
-		} else if();
-{
-  console.log('Not a valid operator');
-};
-return console.log('${num1} ${operator} ${num2} = ${result}' )
-
+const notANumber = function(input) {
+	const validatedInput = {operation: input.operation}
+  validatedInput.num1 = parseInt(input.num1)
+  validatedInput.num2 = parseInt(input.num2)
+  if (isNaN(validatedInput.num1)){
+    console.log(`${input.num1} is not a number.`);
+    return null;
+  };
+  if (isNaN(validatedInput.num2)){
+    console.log(`${input.num2} is not a number.`);
+    return null;
+  };
+  operatorCheck(validatedInput)
 };
 
+const operatorCheck = function(input){
+  if (input.operation === '+' || input.operation === 'add'){
+    return calculateAdd(input);
+};
+  if (input.operation === '-' || input.operation === 'subtract'){
+    return calculateSub(input);
+  };
+  if (input.operation === '/' || input.operation === 'divide'){
+    return calculateDiv(input);
+  };
+  if (input.operation === '*' || input.operation === 'multiply'){
+    return calculateMult(input);
+  };
+	return console.log('unsupported operator')
+};
+const calculateUserInput = function(errors, input){
+  notANumber(input)
+};
 // This exports the function so index.js can import it.
 exports.calculateUserInput = calculateUserInput;
