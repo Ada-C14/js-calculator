23cbba5acb3e9894615dd36584ab28f8be9c9cd2
Lina Do <l.do5147@gmail.com>
2020-12-09 09:29:32 -0800

    changed let to const

--- calculator.js
+++ calculator.js
@@ -25,39 +25,6 @@ const divideNums = function(a, b) {
 };
 
 
-// const calculate = function(input) {
-//   let operation = input.operation;
-//   let firstNum = parseInt(input.num1);
-//   let secondNum = parseInt(input.num2);
-
-//   if (isNaN(firstNum) || isNaN(secondNum) ) {
-//     console.log('Please enter a valid number!');
-//   } else {
-//     switch (operation) {
-//       case 'add': 
-//       case '+':
-//         console.log(addNums(firstNum, secondNum));
-//         break;
-//       case 'subtract': 
-//       case '-':
-//         console.log(subtractNums(firstNum, secondNum));
-//         break;
-//       case 'multiply': 
-//       case '*':
-//         console.log(multiplyNums(firstNum, secondNum));
-//         break;
-//       case 'divide': 
-//       case '/':
-//         console.log(divideNums(firstNum, secondNum));
-//         break;
-//       default:
-//         console.log('unsupported operator');
-//     }
-//   }
-// };
-
-// calculate(userInput)
-
 const calculateUserInput = function (error, promptInput) {
   // console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
 
@@ -67,9 +34,9 @@ const calculateUserInput = function (error, promptInput) {
   // How do we read values from it? What syntax?
   // How can we use it?
   // Can we call our existing functions now, inside of this function?
-  let operation = promptInput.operation;
-  let firstNum = parseInt(promptInput.num1);
-  let secondNum = parseInt(promptInput.num2);
+  const operation = promptInput.operation;
+  const firstNum = parseInt(promptInput.num1);
+  const secondNum = parseInt(promptInput.num2);
 
   if (isNaN(firstNum) || isNaN(secondNum) ) {
     console.log('Please enter a valid number!');
