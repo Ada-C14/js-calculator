c82c571a9e39791946050e5e9101c8cedf27beaa
Lina Do <l.do5147@gmail.com>
2020-12-08 23:04:21 -0800

    added semi-colons and changed some error messages

--- calculator.js
+++ calculator.js
@@ -1,82 +1,82 @@
-const exampleAdditionInput = {
-  num1: 'hi there',
-  num2: 0,
-  operation: 'hello',
-}
+// const exampleAdditionInput = {
+//   num1: 30,
+//   num2: 10,
+//   operation: 'subtract',
+// }
 
 const addNums = function(a, b) {
   return `${a} + ${b} = ${a + b}`;
-}
+};
 
 const subtractNums = function(a, b) {
   return `${a} - ${b} = ${a - b}`;
-}
+};
 
 const multiplyNums = function(a, b) {
   return `${a} * ${b} = ${a * b}`;
-}
+};
 
 const divideNums = function(a, b) {
   if (b === 0) {
-    return 'You cannot divide by 0'
+    return 'You cannot divide by zero'
   } else {
   return `${a} / ${b} = ${a / b}`;
   }
-}
+};
 
-// const isNumber = function(a, b) {
-//   if (isNaN(a)) {
-//     return `${a} is not a number`
-//   } elsif 
-// }
 
 const calculate = function(input) {
   let operation = input.operation;
-  let firstNum = input.num1;
-  let secondNum = input.num2;
+  let firstNum = parseInt(input.num1);
+  let secondNum = parseInt(input.num2);
 
-  switch (operation) {
-    case 'add': 
-    case '+':
-      console.log(addNums(firstNum, secondNum));
-      break;
-    case 'subtract': 
-    case '-':
-      console.log(subtractNums(firstNum, secondNum));
-      break;
-    case 'multiply': 
-    case '*':
-      console.log(multiplyNums(firstNum, secondNum));
-      break;
-    case 'divide': 
-    case '/':
-      console.log(divideNums(firstNum, secondNum));
-      break;
-    default:
-      console.log('unsupported operator');
+  if (isNaN(firstNum) || isNaN(secondNum) ) {
+    console.log('Please enter a valid number!');
+  } else {
+    switch (operation) {
+      case 'add': 
+      case '+':
+        console.log(addNums(firstNum, secondNum));
+        break;
+      case 'subtract': 
+      case '-':
+        console.log(subtractNums(firstNum, secondNum));
+        break;
+      case 'multiply': 
+      case '*':
+        console.log(multiplyNums(firstNum, secondNum));
+        break;
+      case 'divide': 
+      case '/':
+        console.log(divideNums(firstNum, secondNum));
+        break;
+      default:
+        console.log('unsupported operator');
+    }
   }
+};
+
+// calculate(exampleAdditionInput)
+
+const calculateUserInput = function (error, promptInput) {
+  // console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
+
+  // Questions to ask and answer:
+  // What is promptInput?
+  // What data type? What does it hold? What does it represent?
+  // How do we read values from it? What syntax?
+  // How can we use it?
+  // Can we call our existing functions now, inside of this function?
+  calculate(promptInput)
 }
 
-calculate(exampleAdditionInput)
-
-// const calculateUserInput = function (error, promptInput) {
-//   console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
-
-//   // Questions to ask and answer:
-//   // What is promptInput?
-//   // What data type? What does it hold? What does it represent?
-//   // How do we read values from it? What syntax?
-//   // How can we use it?
-//   // Can we call our existing functions now, inside of this function?
-// }
-
-// // Example manual testing of calculator.  
-// // calculateUserInput({}, {
-// //   num1: 3,
-// //   num2: 4,
-// //   operation: 'add',
-// // });
+// Example manual testing of calculator.  
+// calculateUserInput({}, {
+//   num1: 3,
+//   num2: 4,
+//   operation: 'add',
+// });
 
 
-// // This exports the function so index.js can import it.
-// exports.calculateUserInput = calculateUserInput;
+// This exports the function so index.js can import it.
+exports.calculateUserInput = calculateUserInput;
