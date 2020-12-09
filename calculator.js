fcd6eff8c20f90519a7f12c63c1f4fe7c07fbe91
marks214 <aimee.oz.x@gmail.com>
2020-12-08 21:58:39 -0800

    refactored code, removed comments - see previous commits for answers to questions

--- calculator.js
+++ calculator.js
@@ -1,80 +1,74 @@
-
-const exampleAdditionInput = {
-  num1: 3,
-  num2: 5,
-  operation: 'add',
-}
+const allowableOperators = [
+  'add',
+  '+',
+  'subtract', 
+  '-', 
+  'multiply', 
+  '*', 
+  'divide', 
+  '-'
+];
 
 const calculateUserInput = function (error, promptInput) {
-  console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
 
-  // Questions to ask and answer:
-  // What is promptInput? 
-    /* promptInput is a parameter of the calculateUserInput function. */
-  // What data type? What does it hold? What does it represent? 
-    /* The data type is an object. In general, objects are a collection of properties. Here promptInput is a collection of the prompts given to the user and the user's input. For example, after running npm start in the terminal the user is prompted to enter num1, num2 and an operation. If the user enters 1, 2, and add, respectively, then the promptInput will display back { num1: '1', num2: '2', operation: 'add' }. */
-  // How do we read values from it? What syntax?
-    /* We can read values from it like so:
-    promptInput - this will give back the whole object
-    promptInput.num1 or promptInput['num1'] - this will give back the value of (the attribute?) num1. (replace num1 w/ num2 to get back num2 info)
-    promptInput.operation or promptInput['operation'] - this will give back the operation the user has selected */
-  // How can we use it? 
-    /* We can use it to retrieve user entered information. */
-  // Can we call our existing functions now, inside of this function? 
-    /* #yes ↘👾🔥👾🔥👾↙ */
+  const operation = promptInput.operation
 
-  const allowableOperators = [
-    'add',
-    '+', 
-    'subtract', 
-    '-', 
-    'multiply', 
-    '*', 
-    'divide', 
-    '-'
-  ];
+  validations(promptInput.num1, promptInput.num2, operation)
 
-  if (isNaN(promptInput.num1)) {
-    console.log(`${promptInput.num1} is not a number`);
-    return 
-  } 
-  else if (isNaN(promptInput.num2)) {
-    console.log(`${promptInput.num2} is not a number`);
+  const num1 = parseFloat(promptInput.num1)
+  const num2 = parseFloat(promptInput.num2)
+
+  if (operation == 'add' || operation == '+') {
+    addition(num1, num2);
+  } else if (operation == 'subtract' || operation == '-') {
+    subtraction(num1, num2);
+  } else if (operation == 'multiply' || operation == '*') {
+    multiplication(num1, num2);
+  } else if (operation == 'divide' || operation == '/') {
+    division(num1, num2)
+    }
+}
+
+const addition = function(num1, num2) {
+  console.log(`${num1} + ${num2} = ${num1 + num2}`);
+  return
+}
+
+const subtraction = function(num1, num2) {
+  console.log(`${num1} - ${num2} = ${num1 - num2}`);
+  return
+}
+
+const multiplication = function(num1, num2) {
+  console.log(`${num1} * ${num2} = ${num1 * num2}`);
+  return
+}
+
+const division = function(num1, num2) {
+  if (num2 == 0) {
+    console.log('You cannot divide by zero');
+    return }
+  else {
+    console.log(`${num1} / ${num2} = ${num1 / num2}`);
     return
   }
-  else if (!allowableOperators.includes(promptInput.operation )) {
+}
+
+const validations = function(num1, num2, operation) {
+  if (isNaN(num1)) {
+    console.log(`${num1} is not a number`);
+    return 
+  } 
+  else if (isNaN(num2)) {
+    console.log(`${num2} is not a number`);
+    return
+  }
+  else if (!allowableOperators.includes(operation )) {
     console.log('unsupported operator');
     return
   }
 
-
-  if (promptInput.operation == 'add' || promptInput.operation == '+') {
-    console.log(parseFloat(promptInput.num1) + parseFloat(promptInput.num2));
-    return
-  } else if (promptInput.operation == 'subtract' || promptInput.operation == '-') {
-    console.log(parseFloat(promptInput.num1) - parseFloat(promptInput.num2));
-    return
-  } else if (promptInput.operation == 'multiply' || promptInput.operation == '*') {
-    console.log(parseFloat(promptInput.num1) * parseFloat(promptInput.num2));
-    return
-  } else if (promptInput.operation == 'divide' || promptInput.operation == '/') { 
-      if (parseFloat(promptInput.num2) == 0) {
-        console.log('You cannot divide by zero');
-        return }
-      else {
-        console.log(parseFloat(promptInput.num1) / parseFloat(promptInput.num2));
-        return
-      }
-    }
 }
-
-// Example manual testing of calculator.  
-// calculateUserInput({}, {
-//   num1: 3,
-//   num2: 4,
-//   operation: 'add',
-// });
-
 
 // This exports the function so index.js can import it.
 exports.calculateUserInput = calculateUserInput;
