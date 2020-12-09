b5d88d2c3414bf4ab4346f4465457838b6d4119b
Madeline <madelign@gmail.com>
2020-12-09 12:27:41 -0500

    added validations for dividing by 0 and invalid input. fixed switch statement. passes all tests now

--- calculator.js
+++ calculator.js
@@ -47,54 +47,74 @@ const exampleDivisionSymbolInput = {
 }
 
 const calculateUserInput = function (error, promptInput) {
-  console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
+  // console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
 
-  switch(promptInput.operation) {
-    case "add":
-    case "+":
-      return promptInput.num1 + promptInput.num2;
+  const num1 = Number.parseFloat(promptInput.num1);
+  const num2 = Number.parseFloat(promptInput.num2);
 
-    case "subtract":
-    case "-":
-      return promptInput.num1 - promptInput.num2;
+  if (Number.isNaN(num1)) {
+    console.error(`${num1} is not a number.`);
+  } else if (Number.isNaN(num2)) {
+    console.error(`${num2} is not a number`);
+  }
 
-    case "multiply":
-    case "*":
-      return promptInput.num1 * promptInput.num2;
-    
-    case "divide":
-    case "/":
-      return promptInput.num1 / promptInput.num2;
+  switch (promptInput.operation) {
+    case 'add':
+    case '+': {
+      const result = num1 + num2;
+      console.log(`${num1} + ${num2} = ${result}`);
+      return result;
+    }
 
+    case 'subtract':
+    case '-': {
+      const result = num1 - num2;
+      console.log(`${num1} - ${num2} = ${result}`);
+      return result;
+    }
+
+    case 'multiply':
+    case '*': {
+      const result = num1 * num2;
+      console.log(`${num1} * ${num2} = ${result}`);
+      return result;
+    }
+
+    case 'divide':
+    case '/': 
+    if (num2 === 0) {
+      console.log('You cannot divide by zero')
+      return false;
+    } else {
+      const result = num1 / num2;
+      console.log(`${num1} / ${num2} = ${result}`);
+      return result;
+    }
+      break;
     default:
-      console.error(`${promptInput.operation} is not supported.`);
+      console.log(`${promptInput.operation} is not supported.`);
   }
 
   // Questions to ask and answer:
   // What is promptInput? 
-    // Object that has the input
+  // Object that has the input
   // What data type? What does it hold? What does it represent?
-    // The data type is an object. It holds information about the two numbers and which operation is used. It represents user input.
+  // The data type is an object. It holds information about the two numbers and which operation is used. It represents user input.
   // How do we read values from it? What syntax?
-    // Bracket notation or dot notation. Ex: objectName.propertyName or objectName['propertyName']
+  // Bracket notation or dot notation. Ex: objectName.propertyName or objectName['propertyName']
   // How can we use it?
+  // We can use it to access properties of the input. 
   // Can we call our existing functions now, inside of this function?
+    // Yes
 }
 
 // Example manual testing of calculator.  
-// calculateUserInput({}, {
-//   num1: 3,
-//   num2: 4,
-//   operation: 'add',
-// });
+calculateUserInput({}, {
+  num1: 3,
+  num2: 4,
+  operation: 'add',
+});
 
 
 // This exports the function so index.js can import it.
 exports.calculateUserInput = calculateUserInput;
-
-
-output.forEach( function(element) {
-  console.log(``${element}`);
-  return true;
-}
-};
