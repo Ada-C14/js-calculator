65feeb7c3270b23bbfe7eb14009adf1e7eba5c80
Lina Do <l.do5147@gmail.com>
2020-12-09 09:28:09 -0800

    moved calculator function into the calculatorUserInput function

--- calculator.js
+++ calculator.js
@@ -25,10 +25,51 @@ const divideNums = function(a, b) {
 };
 
 
-const calculate = function(input) {
-  let operation = input.operation;
-  let firstNum = parseInt(input.num1);
-  let secondNum = parseInt(input.num2);
+// const calculate = function(input) {
+//   let operation = input.operation;
+//   let firstNum = parseInt(input.num1);
+//   let secondNum = parseInt(input.num2);
+
+//   if (isNaN(firstNum) || isNaN(secondNum) ) {
+//     console.log('Please enter a valid number!');
+//   } else {
+//     switch (operation) {
+//       case 'add': 
+//       case '+':
+//         console.log(addNums(firstNum, secondNum));
+//         break;
+//       case 'subtract': 
+//       case '-':
+//         console.log(subtractNums(firstNum, secondNum));
+//         break;
+//       case 'multiply': 
+//       case '*':
+//         console.log(multiplyNums(firstNum, secondNum));
+//         break;
+//       case 'divide': 
+//       case '/':
+//         console.log(divideNums(firstNum, secondNum));
+//         break;
+//       default:
+//         console.log('unsupported operator');
+//     }
+//   }
+// };
+
+// calculate(userInput)
+
+const calculateUserInput = function (error, promptInput) {
+  // console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
+
+  // Questions to ask and answer:
+  // What is promptInput?
+  // What data type? What does it hold? What does it represent?
+  // How do we read values from it? What syntax?
+  // How can we use it?
+  // Can we call our existing functions now, inside of this function?
+  let operation = promptInput.operation;
+  let firstNum = parseInt(promptInput.num1);
+  let secondNum = parseInt(promptInput.num2);
 
   if (isNaN(firstNum) || isNaN(secondNum) ) {
     console.log('Please enter a valid number!');
@@ -54,20 +95,6 @@ const calculate = function(input) {
         console.log('unsupported operator');
     }
   }
-};
-
-calculate(userInput)
-
-const calculateUserInput = function (error, promptInput) {
-  // console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
-
-  // Questions to ask and answer:
-  // What is promptInput?
-  // What data type? What does it hold? What does it represent?
-  // How do we read values from it? What syntax?
-  // How can we use it?
-  // Can we call our existing functions now, inside of this function?
-  calculate(promptInput)
 }
 
 // Example manual testing of calculator.  
@@ -77,6 +104,6 @@ const calculateUserInput = function (error, promptInput) {
 //   operation: 'add',
 // });
 
-
+calculateUserInput({}, userInput)
 // This exports the function so index.js can import it.
 exports.calculateUserInput = calculateUserInput;
