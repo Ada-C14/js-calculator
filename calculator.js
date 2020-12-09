1d2c9d2562a84ed3f6e443d7493e7064c6d8d723
OlgaSe <olga.tka4eva@gmail.com>
2020-12-08 21:35:49 -0800

    all tests passed

--- calculator.js
+++ calculator.js
@@ -6,8 +6,9 @@ const exampleAdditionInput = {
 }
 // '3 + 5 = 8'
 
+
 const calculateUserInput = function (error, promptInput) {
-  console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
+  // console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
 
   // Questions to ask and answer:
   // What is promptInput?
@@ -15,7 +16,71 @@ const calculateUserInput = function (error, promptInput) {
   // How do we read values from it? What syntax?
   // How can we use it?
   // Can we call our existing functions now, inside of this function?
+
+  // if (promptInput.operation === 'add') {
+  //   add(promptInput.num1, promptInput.num2); 
+  // } else if (promptInput.operation === 'multiply') 
+  // { multiply(promptInput.num1, promptInput.num2) }
+  // else if (promptInput.operation === 'subtract') {
+  //   subtract(promptInput.num1, promptInput.num2)
+  // }
+
+    if (isNaN(promptInput.num1)) {
+      console.log(`${promptInput.num1} is not a number.`);
+      return;
+    } 
+    if (isNaN(promptInput.num2)) {
+      console.log(`${promptInput.num2} is not a number.`);
+      return;
+    }
+
+  let result;
+  switch (promptInput.operation) {
+    case 'add':
+    case '+':
+      result = add(promptInput.num1, promptInput.num2);
+      console.log(`${promptInput.num1} + ${promptInput.num2} = ${result}`);
+      break;
+    case 'multiply':
+    case '*':
+      result = multiply(promptInput.num1, promptInput.num2);
+      console.log(`${promptInput.num1} * ${promptInput.num2} = ${result}`);
+      break;
+    case 'subtract':
+    case '-':
+      result = subtract(promptInput.num1, promptInput.num2);
+      console.log(`${promptInput.num1} - ${promptInput.num2} = ${result}`);
+      break;
+    case 'divide':
+    case '/':
+      if (Number(promptInput.num2) === 0) {
+        console.log('You cannot divide by zero');
+        return;
+      }
+      result = divide(promptInput.num1, promptInput.num2);
+      console.log(`${promptInput.num1} / ${promptInput.num2} = ${result}`);
+      break;
+    default:
+      console.log(`unsupported operator`);
+  }
 }
+
+
+const add = function(num1, num2) {
+  return Number(num1) + Number(num2);
+};
+
+const multiply = function(num1, num2) {
+  return num1 * num2;
+};
+
+const subtract = function(num1, num2) {
+  return num1 - num2;
+};
+
+const divide = function(num1, num2) {
+  return num1 / num2;
+};
 
 // Example manual testing of calculator.  
 calculateUserInput({}, {
