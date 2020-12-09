7f725538f0df1954f959eea6b50e383d78b367ac
Taylor Tompkins <taylormililani@gmail.com>
2020-12-09 11:39:27 -0800

    added missing semicolons

--- calculator.js
+++ calculator.js
@@ -8,24 +8,24 @@ const exampleAdditionInput = {
 const calculateUserInput = function (error, promptInput) {
   // console.log('This is the value of the promptInput variable that got passed in by prompt, after our prompt package collect user input', promptInput);
   const reg = /\(\d[-+\/*]\d\)/;
-  let num1 = ''
-  let num2 = ''
+  let num1 = '';
+  let num2 = '';
   if ((reg.test(promptInput.num1) === true) && (reg.test(promptInput.num2) === true)) {
-    num1 = eval(promptInput.num1) 
-    num2 = eval(promptInput.num2)
+    num1 = eval(promptInput.num1); 
+    num2 = eval(promptInput.num2);
   } else if ((reg.test(promptInput.num1) === true) && (reg.test(promptInput.num2) === false)) {
-    num1 = eval(promptInput.num1)
-    num2 = Number(promptInput.num2)
+    num1 = eval(promptInput.num1);
+    num2 = Number(promptInput.num2);
   } else if ((reg.test(promptInput.num2) === true) && (reg.test(promptInput.num1) === false)) {
-    num2 = eval(promptInput.num2)
-    num1 = Number(promptInput.num1)
+    num2 = eval(promptInput.num2);
+    num1 = Number(promptInput.num1);
   } else {
     if (isNaN(Number(promptInput.num1)) && isNaN(Number(promptInput.num2))) {
-      return console.log(`${ promptInput.num1 } and ${ promptInput.num2 } are not numbers`)
+      return console.log(`${ promptInput.num1 } and ${ promptInput.num2 } are not numbers`);
     } else if (isNaN(Number(promptInput.num1))) {
-      return console.log(`${ promptInput.num1 } is not a number`)
+      return console.log(`${ promptInput.num1 } is not a number`);
     } else if (isNaN(Number(promptInput.num2))) {
-      return console.log(`${ promptInput.num2 } is not a number`)
+      return console.log(`${ promptInput.num2 } is not a number`);
     } 
     num1 = Number(promptInput.num1);
     num2 = Number(promptInput.num2);
@@ -34,23 +34,23 @@ const calculateUserInput = function (error, promptInput) {
   const operation = promptInput.operation.toLowerCase();
 
   if (operation === 'add' || operation === '+') {
-    console.log(`${ promptInput.num1 } + ${ promptInput.num2 } = ` + (num1+num2))
+    console.log(`${ promptInput.num1 } + ${ promptInput.num2 } = ` + (num1+num2));
   } else if (operation === 'subtract' || operation === '-') {
-    console.log(`${ promptInput.num1 } - ${ promptInput.num2 } = ` + (num1-num2))
+    console.log(`${ promptInput.num1 } - ${ promptInput.num2 } = ` + (num1-num2));
   } else if (operation === 'multiply' || operation === '*') {
-    console.log(`${ promptInput.num1 } * ${ promptInput.num2 } = ` + (num1*num2))
+    console.log(`${ promptInput.num1 } * ${ promptInput.num2 } = ` + (num1*num2));
   } else if (operation === 'divide' || operation === '/') {
     if (num2 === 0) {
-      console.log('You cannot divide by zero')
+      console.log('You cannot divide by zero');
     } else {
-      console.log(`${ promptInput.num1 } / ${ promptInput.num2 } = ` + (num1/num2))
+      console.log(`${ promptInput.num1 } / ${ promptInput.num2 } = ` + (num1/num2));
     }
   } else if (operation === 'modulo' || operation === '%') {
-    console.log(`${ promptInput.num1 } % ${ promptInput.num2 } = ` + (num1%num2))
+    console.log(`${ promptInput.num1 } % ${ promptInput.num2 } = ` + (num1%num2));
   } else if (operation === 'exponent' || operation === '**') {
-    console.log(`${ promptInput.num1 } ** ${ promptInput.num2 } = ` +  exponent(num1,num2) + (num1**num2))
+    console.log(`${ promptInput.num1 } ** ${ promptInput.num2 } = ` +  exponent(num1,num2) + (num1**num2));
   } else {
-    console.log('invalid operation')
+    console.log('invalid operation');
   }
   // Questions to ask and answer:
   // What is promptInput? an object 
